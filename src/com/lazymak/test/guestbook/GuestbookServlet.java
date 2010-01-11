fc8d7f542a796b6e032be0fd7cdabf8fb15344c3
makoto_fujimura <makoto_fujimura@10.0.1.3>
2010-01-11 19:29:55 +0900

    home test

--- src/com/lazymak/test/guestbook/GuestbookServlet.java
+++ src/com/lazymak/test/guestbook/GuestbookServlet.java
@@ -8,6 +8,6 @@ public class GuestbookServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, japan");
+		resp.getWriter().println("Hello, universe");
 	}
 }
